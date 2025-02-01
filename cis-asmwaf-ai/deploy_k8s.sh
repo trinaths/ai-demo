048569c94c3396ac80eef183dcd504dcdb67e74c
Trinath SOMANCHI <t.somanchi@f5.com>
2025-02-01 12:28:33 +0530

    changes

--- cis-asmwaf-ai/deploy_k8s.sh
+++ cis-asmwaf-ai/deploy_k8s.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/bash
 kubectl create ns ai-workloads
-kubectl apply -f ai-agent-role.yaml -f cis-deployment.yaml -f as3_configmap.yaml -f ai_model_deploy.yaml -f ai_agent_deploy.yaml
 kubectl create secret generic bigip-login  -n kube-system --from-literal=username=admin  --from-literal=password=admin
-kubectl create secret generic ai-agent-kubeconfig \
-  --from-file=kubeconfig=$HOME/.kube/config \
-  -n ai-workloads
+kubectl create secret generic ai-agent-kubeconfig  --from-file=kubeconfig=$HOME/.kube/config  -n ai-workloads
+kubectl apply -f ai-agent-role.yaml -f cis-deployment.yaml -f as3_configmap.yaml -f ai_model_deploy.yaml -f ai_agent_deploy.yaml
 kubectl get svc,deploy,po -n ai-workloads
