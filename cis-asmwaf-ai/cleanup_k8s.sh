68bd6664e94516167e5cedbf61ed58d8007f771c
Trinath SOMANCHI <t.somanchi@f5.com>
2025-02-01 11:53:13 +0530

    correct delete

--- cis-asmwaf-ai/cleanup_k8s.sh
+++ cis-asmwaf-ai/cleanup_k8s.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 kubectl delete -f ai-agent-role.yaml -f cis-deployment.yaml -f as3_configmap.yaml -f ai_model_deploy.yaml -f ai_agent_deploy.yaml
-kubectl delete secret generic bigip-login  -n kube-system --from-literal=username=admin  --from-literal=password=admin
 kubectl delete secret bigip-login  -n kube-system
 kubectl delete secret ai-agent-kubeconfig -n ai-workloads
 kubectl delete ns ai-workloads
