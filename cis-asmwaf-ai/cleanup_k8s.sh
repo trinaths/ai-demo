593932023c1335b8f26704a7b5740b0723946870
Trinath SOMANCHI <t.somanchi@f5.com>
2025-02-01 20:36:30 +0530

    removed old files

--- cis-asmwaf-ai/cleanup_k8s.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-kubectl delete -f ai-agent-role.yaml -f cis-deployment.yaml -f as3_configmap.yaml -f ai_model_deploy.yaml -f ai_agent_deploy.yaml
-kubectl delete secret bigip-login  -n kube-system
-kubectl delete secret ai-agent-kubeconfig -n ai-workloads
-kubectl delete ns ai-workloads
