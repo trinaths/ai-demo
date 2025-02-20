cdeadf12ea3e275ca41fdcedb6e95392e61205c3
Trinath SOMANCHI <t.somanchi@f5.com>
2025-02-20 17:15:06 +0530

    improve cleanup script

--- bigip-ai-demo/cleanup.sh
+++ bigip-ai-demo/cleanup.sh
@@ -5,4 +5,6 @@ set -e
 NAMESPACE="bigip-demo"
 echo "Deleting namespace '$NAMESPACE' and all its resources..."
 kubectl delete namespace "$NAMESPACE"
+kubectl delete -f k8s/cis.yaml
+kubectl delete secret bigip-login -n kube-system
 echo "Cleanup complete."
