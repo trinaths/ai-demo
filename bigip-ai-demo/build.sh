be2b7297cec252fb156f72e7233a273a82fc859e
Trinath SOMANCHI <t.somanchi@f5.com>
2025-02-20 18:25:47 +0530

    code changes

--- bigip-ai-demo/build.sh
+++ bigip-ai-demo/build.sh
@@ -7,18 +7,18 @@ AI_WORKLOAD_IMAGE="$DOCKER_REGISTRY/ai-workload-sim:latest"
 
 # Build and push Model Service image.
 cd model
-docker build -t "$MODEL_SERVICE_IMAGE" .
+docker build --no-cache  -t "$MODEL_SERVICE_IMAGE" .
 docker push "$MODEL_SERVICE_IMAGE"
 cd ..
 
 # Build and push Agent Service image.
 cd agent
-docker build -t "$AGENT_SERVICE_IMAGE" .
+docker build --no-cache  -t "$AGENT_SERVICE_IMAGE" .
 docker push "$AGENT_SERVICE_IMAGE"
 cd ..
 
 # Build and push AI Workload Simulator image.
 cd ai_workload_simulator
-docker build -t "$AI_WORKLOAD_IMAGE" .
+docker build --no-cache  -t "$AI_WORKLOAD_IMAGE" .
 docker push "$AI_WORKLOAD_IMAGE"
 cd ..
