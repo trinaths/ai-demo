6870964a737c3b437ebd9fab694439c642175cd2
Trinath SOMANCHI <t.somanchi@f5.com>
2025-02-21 17:40:44 +0530

    code changes

--- bigip-ai-demo/cleanup.sh
+++ bigip-ai-demo/cleanup.sh
@@ -4,7 +4,7 @@
 set -e
 NAMESPACE="bigip-demo"
 echo "Deleting all demo resources..."
-kubectl delete -f k8s/pvc.yaml
 kubectl delete namespace "$NAMESPACE" --force --grace-period=0
+kubectl delete -f k8s/pvc.yaml
 kubectl delete -f k8s/cis.yaml
 echo "Cleanup complete."
