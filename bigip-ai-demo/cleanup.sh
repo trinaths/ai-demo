43843af079c5febaffeac691d76acd28f230f900
Trinath SOMANCHI <t.somanchi@f5.com>
2025-02-21 17:18:29 +0530

    cleanup changes

--- bigip-ai-demo/cleanup.sh
+++ bigip-ai-demo/cleanup.sh
@@ -4,11 +4,6 @@
 set -e
 NAMESPACE="bigip-demo"
 echo "Deleting all demo resources..."
-kubectl delete -f k8s/model-service-deployment.yaml --namespace "$NAMESPACE"
-kubectl delete -f k8s/agent-service-deployment.yaml --namespace "$NAMESPACE"
-kubectl delete -f k8s/pvc.yaml --namespace "$NAMESPACE"
-kubectl delete -f k8s/agent-service-rbac.yaml --namespace "$NAMESPACE"
-kubectl delete -f k8s/sample-deployment.yaml --namespace "$NAMESPACE"
-#kubectl apply -f k8s/model-retrain-cron.yaml --namespace "$NAMESPACE"
+kubectl delete namespace "$NAMESPACE"
 kubectl delete -f k8s/cis.yaml
 echo "Cleanup complete."
