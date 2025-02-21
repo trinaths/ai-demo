c8db3bf0e7d1dbdc9787e2c833ce9335ba40a8e7
Trinath SOMANCHI <t.somanchi@f5.com>
2025-02-21 17:41:10 +0530

    no cis

--- bigip-ai-demo/deploy.sh
+++ bigip-ai-demo/deploy.sh
@@ -12,6 +12,6 @@ kubectl apply -f k8s/model-service-deployment.yaml --namespace "$NAMESPACE"
 kubectl apply -f k8s/agent-service-deployment.yaml --namespace "$NAMESPACE"
 kubectl apply -f k8s/sample-deployment.yaml --namespace "$NAMESPACE"
 #kubectl apply -f k8s/model-retrain-cron.yaml --namespace "$NAMESPACE"
-kubectl apply -f k8s/cis.yaml
+#kubectl apply -f k8s/cis.yaml
 echo "Deployment complete. Listing pods in namespace $NAMESPACE:"
 kubectl get pods --namespace "$NAMESPACE"
